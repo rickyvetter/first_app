2ca547b0acfba77a592b6db39572c2e4b8135e75
Ricky <rick_vetter@msn.com>
2013-11-12 23:32:24 -0600

    Initializing Repository

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+
+group :development do
+  gem 'sqlite3'
+end
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'coffee-rails', '~> 4.0.0'
+
+  gem 'uglifier', '>= 1.3.0'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '4dcc8f28417f1b2612158e061d04993b5332ac349e1b34799a8d3a663cd10876b04c136355dfb40a37ffabde316b50aadf53fceed93a49d918590d2db92cb0ac'
